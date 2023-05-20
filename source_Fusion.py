159bcca8653f92569daf7d64a1e97db9a93ddd7e
Rasim Mert YILDIRIM <85447383+gokmavisianka@users.noreply.github.com>
2023-05-20 12:32:01 +0300

    Update source_Fusion.py

--- source_Fusion.py
+++ source_Fusion.py
@@ -1,13 +1,15 @@
+# This file is not in use.
+# Please check the source_MPU6050.py file to see how the "Sensor Fusion" is applied.
+
 class Fusion:
     def __init__(self, sensors):
         self.sensors = sensors
         self.yaw = self.roll = self.pitch = 0
         self.alpha = 0.90
         
-    def update(self, case=0):
-        gyroscope = sensors.MPU6050.gyroscope
-        accelerometer = sensors.MPU6050.accelerometer
-        if case == 2:
-            self.roll = (self.roll + gyroscope.angle.roll) * self.alpha + (acceleration.angle.roll) * (1 - self.alpha)
-            self.pitch = (self.pitch + gyroscope.angle.pitch) * self.alpha + (acceleration.angle.pitch) * (1 - self.alpha)
+    def update(self):
+        gyroscope = self.sensors.MPU6050.gyroscope
+        accelerometer = self.sensors.MPU6050.accelerometer
+        self.roll = (self.roll + gyroscope.angle.roll) * self.alpha + (acceleration.angle.roll) * (1 - self.alpha)
+        self.pitch = (self.pitch + gyroscope.angle.pitch) * self.alpha + (acceleration.angle.pitch) * (1 - self.alpha)
             
